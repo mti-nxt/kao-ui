6bb1091496b39e1925422e430aa5172690c59488
70_10 <7010.dev@gmail.com>
2016-06-04 19:38:16 +0900

    add sample page

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom";
 import React from "react";
 
-import TopContainer from "./containers/top";
+import SampleContainer from "./containers/sample";
 
 ReactDOM.render(
-  <TopContainer />,
+  <SampleContainer />,
   document.getElementById("app")
 );
