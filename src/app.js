5b36650a5d2e858180cf07645a5cae4beae4364c
CircleCI <circleci@cureapp.jp>
2016-06-04 19:54:11 +0900

    release at 2016.06.04 19:54:11

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom";
-import React from "react";
-
-import TopContainer from "./containers/top";
-
-ReactDOM.render(
-  <TopContainer />,
-  document.getElementById("app")
-);
